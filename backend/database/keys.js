6306bd1ecfd217f16f50c1bb708642b89b067656
anderalarcon <20172991@aloe.ulima.edu.pe>
2021-05-10 14:27:33 -0500

    database on cloud deployed

--- backend/database/keys.js
+++ backend/database/keys.js
@@ -1,10 +1,13 @@
 import{Pool} from 'pg';
 const pool=new Pool({
-    host:'localhost',
-    port:'5432',
-    user:'ander',
-    password:'ander',
-    database:'merkaltok_vue'
+    user:"dxhystgervvpex",
+    password:"234982dfd2e2ea026d2178320ba6c31f94db0bf31b197c778282d16385723bba",
+    host:"ec2-107-22-83-3.compute-1.amazonaws.com",
+    port:5432,
+    database:"dfrq6dj08llsm5"  ,
+    ssl: {
+      rejectUnauthorized: false
+    } 
 })
 
 module.exports=pool;
