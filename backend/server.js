d8d80ef15381859cb229a64080121cd7322e2cec
anderalarcon <20172991@aloe.ulima.edu.pe>
2021-05-12 00:07:35 -0500

    to do : car shop

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use("/productos", require('./routes/productos'));
 app.use("/proveedores", require('./routes/proveedor'));
 app.use("/categorias", require('./routes/categorias'));
 app.use("/carrito", require('./routes/carrito'));
-app.use("/Comprobante", require('./routes/Comprobante'));
+/* app.use("/Comprobante", require('./routes/Comprobante')); */
 app.use("/pedidos", require('./routes/pedidos'));
 
 
