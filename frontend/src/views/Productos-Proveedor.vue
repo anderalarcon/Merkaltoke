e810cd3e239ec5dd53f4af167ae78347855c8660
Boris <borisvp123@gmail.com>
2021-05-15 11:03:08 -0500

    Producto-Proveedor_Carrito

--- frontend/src/views/Productos-Proveedor.vue
+++ frontend/src/views/Productos-Proveedor.vue
@@ -20,7 +20,9 @@
           <v-card-subtitle>Stock : {{ producto.stock }} </v-card-subtitle>
           <v-card-subtitle>Detalle : {{ producto.detalle }} </v-card-subtitle>
             <v-card-actions>
-            <v-btn color="blue" small dark>Seleccionar</v-btn>
+            <v-btn color="blue" small dark @click="addToCarrito(producto.id_producto)"
+            >Seleccionar</v-btn
+            >
           </v-card-actions>
         </v-card>
       </v-row>
@@ -31,6 +33,7 @@
 
 <script>
 import Proveedor from "../apis/Proveedor";
+import Carrito from "../apis/Carrito";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 
@@ -41,9 +44,28 @@ export default {
   },
   data: () => ({
     productos: [],
+    user: { role: "", nombre: "", email: "", id: "" },
   }),
 
+  methods: {
+    async addToCarrito(id) {
+      //1.Obtener el carrito id del cliente
+      
+      const consultar_id_cliente = await Carrito.get(`/getCarritoId/${this.user.id}`);
+      const id_carrito=consultar_id_cliente.data.data.cliente.id_carrito;
+     
+      //2. Hacer el insert a la tabla intermedia a penas le da a seleccionar
+
+      const insertarMid = await Carrito.post(`/insertMid/${id_carrito}/${id}`);
+
+      const index = this.productos.findIndex((c) => c.id_producto == 6);
+      this.productos.splice(index, 1); //Desaparecer al instante  asyncrono
+    },
+  },
   created: async function () {
+    this.user = JSON.parse(sessionStorage.getItem("session"));
+    console.log(this.user);
+    
     try {
       const id = this.$route.params.id;
       const res = await Proveedor.get(`/getProductos-Proveedor/${id}`);
