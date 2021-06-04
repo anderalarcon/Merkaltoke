43341f2c2e9e8955e75d49ac2c304801093168d1
Boris <borisvp123@gmail.com>
2021-06-04 09:58:24 -0500

    Indicadores_Backend1

--- frontend/src/views/Proveedor/Indicadores.vue
+++ frontend/src/views/Proveedor/Indicadores.vue
@@ -66,6 +66,7 @@
                               <td>{{ row.item.nombre }}</td>
                               <td>{{ row.item.cantidad }}</td>
                               <td>{{ row.item.precio }}</td>
+                              
                             </tr>
                           </v-data-table>
                           <v-btn
