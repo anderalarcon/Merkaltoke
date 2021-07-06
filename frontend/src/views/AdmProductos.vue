505cd47e63397f33582e01d0358800fc35900afc
Edgar Rodriguez <edgar432910@hotmail.com>
2021-07-05 23:47:57 -0500

    renderizar productos

--- frontend/src/views/AdmProductos.vue
+++ frontend/src/views/AdmProductos.vue
@@ -1,26 +1,67 @@
 <template>
   <div>
     
-    <v-container>
-        <h1>Productos de  {{ user.nombre_proveedor }}</h1>
-      <v-row justify="center">
-          
-        <v-card
-          class="ma-3"
-          max-width="344"
-          v-for="producto in productos"
-          :key="producto.id_producto"
-        >
-          <v-img height="250" v-bind:src="producto.img_producto"> </v-img>
-          <v-card-title>Producto : {{ producto.nombre }} </v-card-title>
+    <NavBarAdmin></NavBarAdmin>
 
-          <v-card-subtitle>Precio : {{ producto.precio }} </v-card-subtitle>
-          <v-card-subtitle>Stock : {{ producto.stock }} </v-card-subtitle>
-          <v-card-subtitle>Detalle : {{ producto.detalle }} </v-card-subtitle>
-          <v-card-subtitle>Visbible? : {{ producto.visible }} </v-card-subtitle>
-          
-        </v-card>
-      </v-row>
+    
+    <v-container class="mt-5">
+      <v-card elevation="15" shaped loading class="pb-3">
+        <v-card-title>
+          Gestión de Productos del Proveedor  {{ user.nombre_proveedor }}
+          <v-spacer></v-spacer>
+         
+        </v-card-title>
+        <v-data-table :headers="headers" :items="productos" >
+          <template v-slot:item="row">
+            <tr>
+              <td>{{ row.item.nombre }}</td>
+              <td>{{ row.item.precio }}</td>
+              <td>{{ row.item.stock }}</td>
+              <td>{{ row.item.detalle }}</td>
+
+              <td>{{ row.item.categoria }}</td>
+
+              <td :id="`visible${row.item.id_producto}`">
+                {{ row.item.visible }}
+              </td>
+
+              <td>
+                <v-btn
+                  color="orange"
+                  x-small
+                  small
+                  dark
+                  fab
+                  class=""
+                  @click="readProductoToUpdate(row.item.id_producto)"
+                >
+                  <v-icon small> mdi-pencil </v-icon></v-btn
+                >
+                <v-btn
+                  color="red"
+                  x-small
+                  small
+                  dark
+                  fab
+                  @click="readProductoToDelete(row.item.id_producto)"
+                >
+                  <v-icon small> mdi-delete </v-icon>
+                </v-btn>
+                <v-btn
+                  color="green"
+                  x-small
+                  small
+                  dark
+                  fab
+                  @click="HacerVisible(row.item.id_producto)"
+                >
+                  <v-icon small> mdi-eye </v-icon>
+                </v-btn>
+              </td>
+            </tr>
+          </template></v-data-table
+        >
+      </v-card>
     </v-container>
     
   </div>
@@ -28,17 +69,37 @@
 
 <script>
 import Proveedor from "../apis/Proveedor";
+import NavBarAdmin from "../components/NavBarAdmin.vue"
 
 
 
 export default {
   name: "Provductos-Proveedor",
   components: {
-   
+   NavBarAdmin,
   },
   data: () => ({
     productos: [],
     user:{},
+     search: "",
+    prueba: '',
+    headers: [
+      {
+        text: "Producto",
+        align: "start",
+        sortable: false,
+        value: "nombre",
+      },
+      { text: "Precio", value: "precio", sortable: true },
+      { text: "Stock", value: "stock" },
+      { text: "Detalle", value: "detalle", sortable: false },
+      { text: "Categoria", value: "categoria", sortable: false },
+
+      { text: "Visible", value: "visible", sortable: false },
+      { text: "Operacion", value: "operacion", sortable: false },
+    ],
+
+    add: false,
     
   }),
 
